0c3d9f0b2da50583b60d98b6b0fd3f5b293ea229
natnapat <natnapatis@gmail.com>
2020-05-01 07:09:29 +0700

    Add files via upload

--- pms_simple.ino
+++ pms_simple.ino
@@ -1,3 +1,7 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); // RS, E, D4, D5, D6, D7
+
+
 byte buf[26];  //serial read buffer
 byte readchar;
 int recieveSum = 0;
@@ -10,9 +14,28 @@ int PM10Value = 0;       //define PM10 value
 char mode = 'a';
 bool toggle = 0;
 char rx_trigger;
+
+int aqical(int pm25){
+  int aqi;
+  int pm_max,pm_min,aqi_max,aqi_min;
+  if(pm25 < 15.5){
+    pm_max = 15.5;
+    pm_min = 0;
+    aqi_max = 51;
+    aqi_min = 0;
+  }
+  else if(pm25 < 40.5){
+    
+  }
+  aqi = ((pm25-pm_min)*(aqi_max-aqi_min)/(pm_max-pm_min))+aqi_min;
+  return aqi;
+  
+}
+
 void setup() {
   Serial.begin(9600);
   Serial1.begin(9600);
+  lcd.begin(16, 2);
 }
 
 // Standby mode. For low power consumption and prolong the life of the sensor.
@@ -89,13 +112,13 @@ void loop()
       }
       readchar = Serial1.read();
       if (readchar == 0x42) { //start reading char if first frame
-        Serial.println();
+        //Serial.println();
         recieveSum = 0;
         counter = 0;
         counterbuf = 0;
       }
-      Serial.print(readchar, HEX);
-      Serial.print(" ");
+      //Serial.print(readchar, HEX);
+      //Serial.print(" ");
       if (counter < 30) {
         recieveSum = recieveSum + readchar;
         if (counter > 3) { //first two bytes are for init, 3rd and 4th are for frame size, not needed for data
@@ -107,7 +130,7 @@ void loop()
         checkSum = readchar; //last two bytes of frame are for checksum
       if (counter == 31) {
         checkSum = (checkSum << 8) + readchar;
-        Serial.print(" CheckSum: "); Serial.print(recieveSum); Serial.print(" "); Serial.println(checkSum);
+        //Serial.print(" CheckSum: "); Serial.print(recieveSum); Serial.print(" "); Serial.println(checkSum);
         if (checkSum == recieveSum) 
           break;
       }
@@ -115,7 +138,7 @@ void loop()
       //delay(2000);
     }
   
-    Serial.println();
+    //Serial.println();
     //  Serial.println(buf[6]<<8);
     //  Serial.println(buf[7]);
   
@@ -143,23 +166,34 @@ void loop()
       *  can be printed.
       */
     if(mode == 'a' || toggle == 1){
-      Serial.println("PM concentration unit under atmospheric environment (ug/m3)");
-      Serial.print("PM   1: ");
-      Serial.print(PM01Value);
-      Serial.print("  ug/m3");
-      Serial.print("\tPM 2.5: ");
-      Serial.print(PM2_5Value);
-      Serial.print("  ug/m3");
-      Serial.print("\tPM  10: ");
-      Serial.print(PM10Value);
-      Serial.print("  ug/m3");
-      Serial.println();
+      //Serial.println("PM concentration unit under atmospheric environment (ug/m3)");
+      //Serial.print("PM   1: ");
+      //Serial.print(PM01Value);
+      //Serial.print("  ug/m3");
+      //Serial.print("\tPM 2.5: ");
+      //Serial.println(PM2_5Value);
+      //Serial.print("  ug/m3");
+//      Serial.print("\tPM  10: ");
+//      Serial.print(PM10Value);
+//      Serial.print("  ug/m3");
+//      Serial.println();
+        int aqi = aqical(PM2_5Value);
+        Serial.println(aqi);
+
+      lcd.setCursor(0, 0);
+      lcd.print("PM2.5Value:");
+      lcd.print(PM2_5Value);
+      lcd.setCursor(0, 1);
+      lcd.print("AQI:");
+      lcd.print(aqi);
+      delay(500);
+      lcd.clear();
     }
     else{
-      Serial.print("sensor is in mode ");
-      Serial.println(mode);
+      //Serial.print("sensor is in mode ");
+      //Serial.println(mode);
     }
     toggle = 0;
-    delay (3000);
+    //delay (3000);
     
 }
